0db484bbd6d10d0cfd392bb5972bab24c2f8d385
7empes7s <125469033+7empes7s@users.noreply.github.com>
2023-03-16 06:30:32 +0100

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-/*
- * main - main block
- * Return: 0
+/**
+ * main - Entry point
+ *
+ * This function is the entry point of the program.
+ * It prints a message to the standard output.
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
